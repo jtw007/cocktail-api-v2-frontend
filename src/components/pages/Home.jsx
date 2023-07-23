a2922768febda7d8f8f5f6fc83bae54acb0a6914
jtw007 <joshwu095@gmail.com>
2023-07-22 17:19:38 -0700

    code only returns empty array in console atm

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -4,22 +4,53 @@ import axios from 'axios'
 import { useState, useEffect } from 'react'
 
 const Home = () => {
-    //display search results on the home page
-    const [results, setResults ] = useState([])
+    const [recipe, setRecipe ] = useState([])
+    const [results, setResults ] = useState('')
+    // const apiNinjaKey = process.env.API_KEY 
 
-    const fetchResults = async (req, res) => {
+    // useEffect(() => {
+    //     const fetchResults = async () => {
+    //         try {
+    //             const response = await fetch(
+    //                 `https://api.api-ninjas.com/v1/cocktail?name=${name}`,
+    //                 {
+    //                     method: "GET",
+    //                     headers: {
+    //                         'X-Api-Key': apiNinjaKey,
+    //                     }
+    //                 }
+    //             )
+    //             const results = await response.json()
+    //             console.log(results)
+    //         } catch (error) {
+    //             console.warn(error)
+    //         }
+    //     }
+    // })
+
+    const fetchResults = async () => {
         try {
-            let name = req.query.search 
-            e.preventDefault()
-            const url = `https://api.api-ninjas.com/v1/cocktail?name=${name}`
+            // const url = `https://api.api-ninjas.com/v1/cocktail?name=${results}`
             const config = { headers: { 'X-Api-Key': API_KEY}} 
-            const response = await axios.get(url, config)
-            setResults(response.data)
+            const response = await axios.get(`https://api.api-ninjas.com/v1/cocktail?name=${results}`, config)
+            setRecipe(response.data)
             console.log(fetchResults)
         } catch (error) {
             console.warn(error)
         }
     }
+
+    const recipeResults = recipe.map((cocktail, idx) => {
+        return (
+            <div key={`cocktail-${idx}`}>
+                <h3>{cocktail.name}</h3>
+                <p>Ingredients: {cocktail.ingredients}</p>
+                <p>Instructions: {cocktail.instructions}</p>
+            </div>
+        )
+    })
+    console.log(recipeResults)
+    
     return (
         <div className='home'>
             <div className='card-body'>
@@ -31,11 +62,28 @@ const Home = () => {
             </div>
 
             <div className="">
-                <form className="" onSubmit={fetchResults}>
-                    <input className="" autocomplete="off" name="search" type="text" placeholder="Search for a recipe" aria-label="Search" />
-                    <button className="" type="submit">Search</button>
+                <form className="">
+                    <input 
+                        className="" 
+                        autoComplete="off" 
+                        name="search" 
+                        type="text" 
+                        placeholder="Search for a recipe" 
+                        aria-label="Search" 
+                        // value={recipe}
+                        onChange={(e) => setResults(e.target.value)}
+                    />
+
+                    <button className="" 
+                        type="submit" 
+                        onClick={fetchResults}
+                    >
+                        Search</button>
                 </form>
             </div>
+
+            {recipeResults}
+
         </div>
     )
 }
