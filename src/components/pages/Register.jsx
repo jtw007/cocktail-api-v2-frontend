dd7c459b3eb8b82fb91e1be6d0b69aae6dde9dd8
jtw007 <joshwu095@gmail.com>
2023-09-19 17:22:01 -0700

    finished first half of register component

--- src/components/pages/Register.jsx
+++ src/components/pages/Register.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import { useState } from 'react'
+import axios from 'axios'
+import jwt_decode from 'jwt-decode'
+import { Navigate } from 'react-router-dom'
 
-const Register = () => {
-  return (
-    <div>Register</div>
-  )
+const Register = ({ currentUser, setCurrentUser }) => {
+    // state for the controlled form
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [msg, setMsg] = useState('')
+
+    // submit event handler
+	const handleSubmit = async e => {
+		e.preventDefault()
+		try {
+			// post fortm data to the backend
+			const reqBody = {
+				name,
+				email, 
+				password
+			}
+			const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/users/register`, reqBody)
+
+			// save the token in localstorage
+			const { token } = response.data
+			localStorage.setItem('jwt', token)
+
+			// decode the token
+			const decoded = jwt_decode(token)
+
+			// set the user in App's state to be the decoded token
+			setCurrentUser(decoded)
+
+		} catch (err) {
+			console.warn(err)
+			if (err.response) {
+				setMsg(err.response.data.msg)
+			}
+		}
+    }
+
+    //conditionally render a navigate component
+    if (currentUser) {
+        return <Navigate to='/profile' />
+    }
+
+    return (
+        <div>Register</div>
+    )
 }
 
 export default Register
