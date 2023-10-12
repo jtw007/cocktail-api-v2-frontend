ad080932ad9b9d78faa0c6649ee216ca71537c0b
jtw007 <joshwu095@gmail.com>
2023-10-11 17:42:18 -0700

    updated navbar for logged in users and not logged in users, still working on login typeerror

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = ({ currentUser, handleLogout }) => {
-    
+    const loggedIn = (
+		<>
+			{/* if the user is logged in... */}
+			<Link to="/">
+				<span onClick={handleLogout}>logout</span>
+			</Link>
+
+			<Link to="/profile">
+				Profile
+			</Link>
+		</>
+	 )
+
+	 const loggedOut = (
+		<>
+			{/* if the user is not logged in... */}
+			<Link to="/register">
+				Register
+			</Link>
+
+			<Link to="/login">
+				Login
+			</Link>
+		</>
+	 )
 
     return (
     <nav className=''>
@@ -10,6 +34,8 @@ const NavBar = ({ currentUser, handleLogout }) => {
             <a className='' href='/profile'>Profile</a>
             <a className='' href='/login'>Login</a>
         </div>
+
+        {currentUser ? loggedIn : loggedOut}
     </nav>
     )
 }
